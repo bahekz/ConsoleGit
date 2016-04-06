ebb41123b7a657b3d027a1aba8814fb5316814a1
Данил Рязанов < >
2016-04-06 22:26:47 +0300

    Danil Ryazanov

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int a, b[10];
+    cin>>a;
+	for(int i=0;i<a;i++)
+	{
+		cin>>b[i];
+	}
+	
+    getch();
+    return 0;
+}
+
